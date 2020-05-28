70c09a1c40100adeab2d77fe6f9253babb53f944
thescripted <somethingscripted@gmail.com>
2020-05-28 14:40:35 -0500

    Added local cache

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,65 @@
 import React from "react";
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
-const Link = ({ link }) => {
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+const Link = (props) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  const _voteForLink = () => {
+    return;
+  };
+
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{props.index + 1}.</span>
+        {authToken && (
+          <Mutation
+            mutation={VOTE_MUTATION}
+            variables={{ linkId: props.link.id }}
+            update={(store, { data: { vote } }) =>
+              props.updateStoreAfterVote(store, vote, props.link.id)
+            }
+          >
+            {(voteMutation) => (
+              <div className="ml1 gray f11" onClick={voteMutation}>
+                ▲
+              </div>
+            )}
+          </Mutation>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {props.link.description} ({props.link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {props.link.votes.length} votes | by{" "}
+          {props.link.postedBy ? props.link.postedBy.name : "Unknown"}{" "}
+          {timeDifferenceForDate(props.link.createdAt)}
+        </div>
       </div>
     </div>
   );
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,7 +3,7 @@ import Link from "./Link";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -11,12 +11,30 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `;
 
 const LinkList = () => {
+  const _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+    const votedLink = data.feeds.link.find((link) => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
   return (
     <Query query={FEED_QUERY}>
       {({ loading, error, data }) => {
@@ -27,8 +45,13 @@ const LinkList = () => {
 
         return (
           <div>
-            {linksToRender.map((link) => (
-              <Link key={link.id} link={link} />
+            {linksToRender.map((link, index) => (
+              <Link
+                key={link.id}
+                link={link}
+                index={index}
+                updateStoreAfterVote={_updateCacheAfterVote}
+              />
             ))}
           </div>
         );
